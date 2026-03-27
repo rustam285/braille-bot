30ee3e2fac8305f2680e66355e4a04a028b511bd
rustam540sg <kamirarsenov@gmail.com>
2026-03-27 20:11:44 +0500

    бритни с пирсом

--- vk_dictation_bot.py
+++ vk_dictation_bot.py
@@ -10,9 +10,7 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
-from dotenv import load_dotenv
 import os
-load_dotenv()
 
 # ─────────────────────────────────────────────
 #  КОНФИГУРАЦИЯ
